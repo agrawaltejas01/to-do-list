ddda6373c96316e819c5eccd373dbdb058ae5e50
sagarbrptr <www.sagarb@gmail.com>
2020-06-02 23:57:04 +0530

    Add task full working, username added everywhere

--- src/app/app-panel/add-task/add-task.component.ts
+++ src/app/app-panel/add-task/add-task.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/store/service/user.service';
-import { EventEmitter } from 'protractor';
+import { EventEmitter, element } from 'protractor';
 import { taskSchema } from 'src/app/store/schema/userTasks-schema';
 import { AddTaskPageService } from 'src/app/store/service/add-task-page-service';
 import { UserTasksService } from 'src/app/store/service/user-tasks-service';
@@ -13,9 +13,15 @@ import { UserTasksService } from 'src/app/store/service/user-tasks-service';
 })
 export class AddTaskComponent implements OnInit
 {
-  labels = ["Personal", "Work", "Shopping", "other"];
-  addTaskForm: FormGroup;
+  @Input() currentTab : String;
+  @Input() taskList: taskSchema[];
 
+  defaultLabels = ["Personal", "Work", "Shopping", "other"];
+  labels = [];
+  priorities = [ { label:"High", value:1 } , { label:"Normal", value:2 } , { label:"Low", value:3 }];
+  
+  addTaskForm: FormGroup;
+  
   constructor(private fb: FormBuilder,
     private userService: UserService,
     public addTaskPageService: AddTaskPageService,
@@ -23,8 +29,19 @@ export class AddTaskComponent implements OnInit
   {
 
   }
+  updateLabels()
+  {
+    this.labels=this.userTasksService.getTaskLabelListArray();
+    
+    //Check whether default labels are already present in Label list for service, only one copy of a label to be present
+    this.defaultLabels.forEach((element)=>{
+      if(this.labels.indexOf(element)==-1)
+        this.labels.push(element);
+    });
+  }
   ngOnInit(): void
   {
+    this.updateLabels();
     this.addTaskForm = this.fb.group({
       tasklabel: ['Personal'],
       tasktitle: ["", Validators.required],
@@ -35,6 +52,12 @@ export class AddTaskComponent implements OnInit
     });
   }
 
+  addToPanelTaskList(newTask : taskSchema){
+    //either its' default tab or it's the same tab
+    if(this.currentTab=="All" || this.currentTab == newTask.label)
+      this.taskList.push(newTask);
+  }
+  
   submitTask()
   {
     if (!this.addTaskForm.valid)
@@ -51,7 +74,7 @@ export class AddTaskComponent implements OnInit
       }
     }
     let userTaskBody = {
-      "username": "Tejas",
+      "username": this.userTasksService.username,
       "task": new taskSchema()
     };
     let newTask = new taskSchema();
@@ -77,6 +100,7 @@ export class AddTaskComponent implements OnInit
       {
         newTask._id = data;
         this.userTasksService.addTask(newTask);
+        this.addToPanelTaskList(newTask);
         this.addTaskPageService.toggleAddTaskPage();
       }
     },
--- src/app/app-panel/update-task/update-task.component.ts
+++ src/app/app-panel/update-task/update-task.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { taskSchema } from 'src/app/store/schema/userTasks-schema';
 import { UserService } from 'src/app/store/service/user.service';
+import { UserTasksService } from 'src/app/store/service/user-tasks-service';
 
 @Component({
   selector: 'app-update-task',
@@ -11,20 +12,62 @@ import { UserService } from 'src/app/store/service/user.service';
 export class UpdateTaskComponent implements OnInit {
 
   @Input() task : taskSchema;
-  labels = ["Personal", "Work", "Shopping", "other"];
+  @Input() currentTab : String;
+  @Input() taskList: taskSchema[];
+
+  defaultLabels = ["Personal", "Work", "Shopping", "other"];
+  labels = [];
   priorities = [ { label:"High", value:1 } , { label:"Normal", value:2 } , { label:"Low", value:3 }];
+  
   updateTaskForm: FormGroup;
   isLoading : Boolean =false;
+  
   constructor(private fb: FormBuilder,
     private userService: UserService,
-    ) {
+    private userTasksService : UserTasksService) 
+  {
       
-     }
+  }
   
+  updateLabels()
+  {
+    this.labels=this.userTasksService.getTaskLabelListArray();
+    
+    //Check whether default labels are already present in Label list for service, only one copy of a label to be present
+    this.defaultLabels.forEach((element)=>{
+      if(this.labels.indexOf(element)==-1)
+        this.labels.push(element);
+    });
+  }
+
   ngOnChanges(){
     // console.log("update input changes",this.task);
     this.ngOnInit();
   }
+
+  updatePanelTaskList(newTask : taskSchema)
+  {
+    let indexOfUpdatedTask = this.taskList.findIndex((element)=>(element._id==newTask._id));   
+    //either its' default tab or it's the same tab
+    if(this.currentTab=="All" || this.currentTab == newTask.label)
+    {
+      if(indexOfUpdatedTask==-1)
+        this.taskList.push(newTask);
+      else
+        this.taskList[indexOfUpdatedTask]=newTask;
+    }
+    else
+    {
+      // Remove from the taskList as label has changed
+      this.taskList.splice(indexOfUpdatedTask,1);  
+    }
+    if(this.taskList.length==0)
+    {
+      // The label is invalid and all the 
+    }
+
+  }
+
   toggleLoading()
   {
     this.isLoading = !this.isLoading;
@@ -32,8 +75,7 @@ export class UpdateTaskComponent implements OnInit {
   ngOnInit(): void
   {
     this.isLoading=false;
-    if(!this.labels.includes(this.task.label)) 
-      this.labels.push(this.task.label);
+    this.updateLabels();  
     this.updateTaskForm = this.fb.group({
       tasklabel: [this.task.label] ,
       tasktitle: [this.task.title, Validators.required],
@@ -61,7 +103,7 @@ export class UpdateTaskComponent implements OnInit {
       }
     }
     let userTaskBody = {
-      "username": "Tejas",
+      "username": this.userTasksService.username,
       "task": new taskSchema()
     };
     let newTask = new taskSchema();
@@ -89,6 +131,9 @@ export class UpdateTaskComponent implements OnInit {
       {
         console.log("Success fully updated");
         this.task=newTask;
+        this.userTasksService.updateTask(newTask);
+        console.log("userTaskServ up");
+        this.updatePanelTaskList(newTask);
         this.ngOnInit();
         // this.task=newTask;
         // this.userTasksService.addTask(newTask);

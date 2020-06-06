24c15b7241612b4ea01333aa8abcb1851a5a6c29
agrawaltejas01 <agrawaltejas01@gmail.com>
2020-06-05 21:31:47 -0700

    app-routing module cleaned

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Component
-import { AppComponent } from './app.component';
 import { PanelComponent } from './app-panel/panel/panel.component';
-import { AddTaskComponent } from './app-panel/add-task/add-task.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterationComponent } from './authentication/registeration/registeration.component';
 
